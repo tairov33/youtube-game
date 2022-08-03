cb97f0bb8e3606b38272bb28ee540a92ca457973
Muhammad <muhammadtairov@icloud.com>
2022-08-03 10:56:42 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyles from './theme/globalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles/>
+    </> 
+  );
+}
+
+export default App;
